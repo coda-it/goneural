5341512f71be6e574f876058b90509d7357c4efa
Oskar Szura <oskar.szura@gmail.com>
2021-03-21 15:59:19 +0100

    CODA-149 - Replace sigmoid with tanh (#2)

--- network_test.go
+++ network_test.go
@@ -5,19 +5,21 @@ import (
 )
 
 func TestNetwork(t *testing.T) {
-	t.Run("Should adjust weights to get closer to the expected value", func(t *testing.T) {
+	t.Run("Should resolve XOR problem after training", func(t *testing.T) {
 		network := Network{
-			LearningRate: 0.1,
+			LearningRate:       0.1,
+			TransferFunction:   Tanh,
+			TransferDerivative: TanhDerivative,
 			layers: []*Layer{
 				&Layer{
 					neurons: []*Neuron{
 						&Neuron{
 							Bias:   0,
-							Weight: []float64{0.5, 0.5},
+							Weight: []float64{0.1, 0.5},
 						},
 						&Neuron{
 							Bias:   0,
-							Weight: []float64{0.5, 0.5},
+							Weight: []float64{1.3, 0.1},
 						},
 					},
 				},
@@ -34,15 +36,41 @@ func TestNetwork(t *testing.T) {
 
 		var outputs []float64
 
-		for i := 0; i < 1000; i++ {
+		for i := 0; i < 100; i++ {
+			outputs = network.Think([]float64{0, 0})
+			network.BackPropagate(outputs, []float64{0})
+		}
+		for i := 0; i < 100; i++ {
 			outputs = network.Think([]float64{0, 1})
+			network.BackPropagate(outputs, []float64{1})
+		}
+		for i := 0; i < 100; i++ {
+			outputs = network.Think([]float64{1, 0})
+			network.BackPropagate(outputs, []float64{1})
+		}
+		for i := 0; i < 100; i++ {
+			outputs = network.Think([]float64{1, 1})
 			network.BackPropagate(outputs, []float64{1})
 		}
 
 		outputs = network.Think([]float64{0, 0})
+		if len(outputs) != 1 && outputs[0] <= 0.1 {
+			t.Errorf("(0,0) XOR not resolved properly")
+		}
 
-		if len(outputs) != 1 && outputs[0] == 0.9684122982829187 {
-			t.Errorf("Output signal is wrongly calculated")
+		outputs = network.Think([]float64{1, 0})
+		if len(outputs) != 1 && outputs[0] >= 0.9 {
+			t.Errorf("(1,0) XOR not resolved properly")
+		}
+
+		outputs = network.Think([]float64{0, 1})
+		if len(outputs) != 1 && outputs[0] >= 0.9 {
+			t.Errorf("(0,1) XOR not resolved properly")
+		}
+
+		outputs = network.Think([]float64{1, 1})
+		if len(outputs) != 1 && outputs[0] >= 0.9 {
+			t.Errorf("(1,1) XOR not resolved properly")
 		}
 	})
 }
--- neuron.go
+++ neuron.go
@@ -1,9 +1,5 @@
 package goneural
 
-import (
-	"math"
-)
-
 // Neuron - single perceptron struct
 type Neuron struct {
 	Bias   float64
@@ -12,18 +8,4 @@ type Neuron struct {
 	Input  float64
 	Error  float64
 	Delta  float64
-}
-
-func (n *Neuron) sigmoid(netSignal float64) float64 {
-	return 1 / (1 + math.Exp(-netSignal))
-}
-
-// SigmoidDerivative - sigmoid derivative used for learning neural network
-func (n *Neuron) SigmoidDerivative(outputSignal float64) float64 {
-	return outputSignal * (1.0 - outputSignal)
-}
-
-// TransferFunction - calculates net signal into gross output signal
-func (n *Neuron) TransferFunction(netSignal float64) float64 {
-	return n.sigmoid(netSignal)
 }
